236a782420ed3495de5797dbc4ff912c8c6fac83
Mayank-wings <mayank.m@wingsts.com>
2023-02-09 17:49:18 +0530

    test.php

---
+++ test.php
@@ -0,0 +1,50 @@
+<?php 
+echo "<h1>Hello Welcome to the php world!</h1>";
+$a1=array("a"=>"red","b"=>"green","c"=>"pink","d"=>"yellow");
+$a2=array("e"=>"red","f"=>"green","g"=>"blue");
+
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+$fname=array("Peter","Ben","Joe");
+$age=array("35","37","43");
+$a = array(
+    array(
+      'id' => 5698,
+      'first_name' => 'Peter',
+      'last_name' => 'Griffin',
+    ),
+    array(
+      'id' => 4767,
+      'first_name' => 'Ben',
+      'last_name' => 'Smith',
+    ),
+    array(
+      'id' => 3809,
+      'first_name' => 'Joe',
+      'last_name' => 'Doe',
+    )
+  );
+$cars=array("Volvo","BMW","Toyota","Honda","Mercedes","Opel");
+
+
+print_r(array_chunk($age,1));
+echo "<br>";
+print_r(array_chunk($age,1,true));
+echo "<br><br> for array column <br>"; 
+print_r(array_column($a,'id'));
+
+echo "<br><br> for array combine array fname and age <br>";
+print_r(array_combine($fname,$age));
+
+echo"<br><br>for the count values<br>";
+print_r(array_count_values($cars));
+
+echo "<br><br>for thr diff values with array_diff<br>";
+print_r(array_diff($a1,$a2));
+
+echo "<br><br>for the diff with key and value with array_diff_assoc <br>";
+print_r(array_diff_assoc($a1,$a2));
+
+
+
+
+?>
